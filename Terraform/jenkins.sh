1b0ff000329c12582a8e065ae348342f1063483e
Nikiforov Viacheslav <55716893+slabuke@users.noreply.github.com>
2020-04-02 20:47:15 +0300

    Update jenkins.sh

--- Terraform/jenkins.sh
+++ Terraform/jenkins.sh
@@ -7,11 +7,10 @@ echo "waiting for internet"
 done
 echo "Internet connection is stable"
 
-sudo yum install git -y
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py -y
-pip install ansible -y
-pip install ansible-lint -y
-sudo yum install python-requests
+yum update -y
+yum install epel-release -y
+yum install git -y
+yum install ansible -y
 git clone https://github.com/mitter91/jenkinsD1.git
 ansible-playbook ./jenkinsD1/ansible/jenkins.yml
+echo "All Done"
